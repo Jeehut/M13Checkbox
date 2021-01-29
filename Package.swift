238bf0f37127ebbf5f5af8830733432f079f09f4
Cihat Gündüz <cg@papershift.com>
2021-01-29 15:01:18 +0100

    Update Swift package manifest for Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,13 +1,16 @@
-//
-//  Package.swift
-//  M13Checkbox
-//
-//  Created by McQuilkin, Brandon on 4/13/16.
-//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
-//
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "M13Checkbox"
+    name: "M13Checkbox",
+    platforms: [.iOS(.v8)],
+    products: [.library(name: "M13Checkbox", targets: ["M13Checkbox"])],
+    dependencies: [],
+    targets: [
+      .target(
+        name: "M13Checkbox",
+        dependencies: [],
+        path: "Sources"
+      ),
+    ]
 )
